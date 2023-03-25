b10a3af6c4acdf93039bd9bbba94a22a14779fef
Tomoya Matsubara <toto.mamama@keio.jp>
2023-03-25 15:53:46 +0900

    feat: start MQTT client subsription when the server starts

--- src/main/kotlin/com/mems/workout/backend/BackendApplication.kt
+++ src/main/kotlin/com/mems/workout/backend/BackendApplication.kt
@@ -1,11 +1,30 @@
 package com.mems.workout.backend
 
+import com.mems.workout.backend.mqtt.Subscriber
+import org.eclipse.paho.client.mqttv3.MqttException
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import kotlin.system.exitProcess
 
 @SpringBootApplication
 class BackendApplication
 
 fun main(args: Array<String>) {
 	runApplication<BackendApplication>(*args)
+
+	val subscriber = Subscriber(
+		"localhost",
+		"mqttTopic",
+	)
+	try {
+		subscriber.subscribe()
+	} catch (me: MqttException) {
+		println("reason: ${me.reasonCode}")
+		println("message: ${me.message}")
+		println("localize: ${me.localizedMessage}")
+		println("cause: ${me.cause}")
+		println("exception: $me")
+
+		exitProcess(1)
+	}
 }
