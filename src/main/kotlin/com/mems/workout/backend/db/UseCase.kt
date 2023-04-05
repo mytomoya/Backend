7909e85ca3bec2214350f0c0ae61de8f4e6939ac
Tomoya Matsubara <toto.mamama@keio.jp>
2023-04-06 01:36:38 +0900

    feat: add `validate()`

--- src/main/kotlin/com/mems/workout/backend/db/UseCase.kt
+++ src/main/kotlin/com/mems/workout/backend/db/UseCase.kt
@@ -1,5 +1,6 @@
 package com.mems.workout.backend.db
 
+import com.fasterxml.jackson.databind.node.ArrayNode
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -9,7 +10,30 @@ class UseCase {
     private lateinit var domain: Domain
 
     fun add(data: Data): Boolean {
+        if (!validate(data)) {
+            return false
+        }
+
         val numUpdated = domain.add(data)
         return numUpdated > 0
+    }
+
+    private fun validate(data: Data): Boolean {
+        val time = data.getDataJson().get("time")
+        val values = data.getDataJson().get("values")
+
+        if (time == null || values == null) {
+            return false
+        }
+        if (!time.isArray || !time.isArray) {
+            return false
+        }
+
+        val timeArray = time as ArrayNode
+        val valuesArray = values as ArrayNode
+        if (timeArray.size() != valuesArray.size()) {
+            return false
+        }
+        return true
     }
 }
