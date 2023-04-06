427a932e13258b100aec690180d3d7b7b74d4b8a
Tomoya Matsubara <toto.mamama@keio.jp>
2023-04-07 02:54:33 +0900

    feat: add response of `/get`

--- src/main/kotlin/com/mems/workout/backend/websocket/Controller.kt
+++ src/main/kotlin/com/mems/workout/backend/websocket/Controller.kt
@@ -2,14 +2,19 @@ package com.mems.workout.backend.websocket
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.mems.workout.backend.db.Data
+import com.mems.workout.backend.db.InvalidIdException
 import com.mems.workout.backend.db.UseCase
 import com.mems.workout.backend.model.Message
 import com.mems.workout.backend.mqtt.Subscriber
 import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -64,14 +69,31 @@ class Controller {
     }
 
     @GetMapping("/get")
-    fun get(@RequestParam("id") id: Int) {
+    fun get(@RequestParam("id") id: Int): ResponseEntity<Map<String, Any>> {
 
         val data = useCase.get(id)
-        if (data != null) {
+        return if (data != null) {
             println("[Success] got: \n$data")
+            ResponseEntity.ok(
+                mapOf(
+                    "data" to data,
+                    "error" to "",
+                ))
         } else {
             println("[Error] getting $id failed")
+            throw InvalidIdException("Invalid ID: $id")
         }
+    }
+
+    @ExceptionHandler(InvalidIdException::class)
+    fun handleInvalidIdException(exception: InvalidIdException): ResponseEntity<Map<String, Any>> {
+        val error = exception.message ?: ""
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(
+                mapOf(
+                    "data" to "",
+                    "error" to error,
+                ))
     }
 
     @PostConstruct
