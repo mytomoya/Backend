770f1975e4b3cf11491e1909a0b20c4fe3fda17f
Tomoya Matsubara <toto.mamama@keio.jp>
2023-04-09 23:04:07 +0900

    refactor: delete `setApplicationDestinationPrefixes()`

--- src/main/kotlin/com/mems/workout/backend/websocket/Config.kt
+++ src/main/kotlin/com/mems/workout/backend/websocket/Config.kt
@@ -11,7 +11,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 class Config : WebSocketMessageBrokerConfigurer {
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
         registry.enableSimpleBroker("/topic")
-        registry.setApplicationDestinationPrefixes("/app")
     }
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
